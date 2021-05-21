cfbdc9b51d4bf1ac3a9c2acb452caead24dd35f6
Isaac Watson <isaac.watson28@gmail.com>
2021-05-21 19:36:15 -0400

    Working on opening a prediction. Issue with locality of variables.

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -23,23 +25,34 @@ async def on_message(message):
         return
     else:
         await update_data(users, user_id)
-    
+
     if message.author.display_name == "OrnateGale":
         emoji = client.get_emoji(718618579918127124)
         await message.add_reaction(emoji)
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-    
+
     if message.content.startswith('::bank'):
         await message.channel.send('You have ' + '${:,}'.format((users[user_id]["money"])))
+
+    
+    pred_open = False
+    if message.content.startswith('::openpred'):
+        if pred_open:
+            await message.channel.send('There is already a prediction open. You can only open one at a time.')
+        elif not pred_open:
+            pred_open = True
+            await message.channel.send('Prediction open. Place your bets')
 
     with open("users.json", "w") as f:
         json.dump(users, f)
 
+
 async def update_data(users, user_id):
     if not user_id in users:
         users[user_id] = {}
-        users[user_id]["money"] = 0       
+        users[user_id]["money"] = 0
+
 
 client.run(TOKEN)
