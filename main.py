204dc051e7354d776a6d7745c35c0875a0e0810a
Mrbeck101 <jamarbeck721@gmail.com>
2021-05-23 22:32:16 -0400

    Added Google Speech Api and pyAudio

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ import discord
 from discord.ext import commands
 import json
 import os
-import speech_recognition as SR
+from google.cloud.speech_v1.types.cloud_speech import RecognizeResponse
+import pyaudio
+import time
+from google.cloud import speech
+import speech_recognition as sr
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GTOKEN = os.getenv("GOOLE_API")
+#credentials = service_account.Credentials.from_service_account_file('GoogleCredentials.json')
+client = speech.SpeechClient.from_service_account_json('GoogleCredentials.json')
+
+r = sr.Recognizer()
+CHUNK=2048
+FORMAT=pyaudio.paInt16
+CHANNELS=1
+RATE=16000
 
 bot = commands.Bot(command_prefix="$", description="A bot")
 
@@ -39,6 +50,41 @@ async def summon(ctx, *args):
             if (ctx.voice_client is None):
                 channel = bot.get_channel(channel_dict[args[0]])
                 await channel.connect()
+
+                p = pyaudio.PyAudio()
+                stream=p.open(
+                    format=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=CHUNK
+                )
+
+                config = speech.RecognitionConfig(
+                    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+                    audio_channel_count=1,
+                    sample_rate_hertz=16000,
+                    language_code="en-US"
+                )
+
+                t_end = time.time() + 10
+
+                while time.time() < t_end:
+                    try:
+                        data=stream.read(CHUNK)
+                        ctx.voice_client.send_audio_packet(data, encode=False)
+                        print(data)
+                    except:
+                        print("failed to send audio packet")
+
+                
+                audio = speech.RecognitionAudio(content=data) 
+                response = client.recognize(config=config, audio=audio)
+
+                for result in response.results:
+                    print("Transcript: {}".format(result.alternatives[0].transcript))
+
             else:
                 await ctx.voice_client.disconnect()
                 channel = bot.get_channel(channel_dict[args[0]])
