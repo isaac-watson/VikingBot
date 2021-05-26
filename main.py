2736dc7429dd747c65be8fc1354c0ed19f23d1c7
Mrbeck101 <jamarbeck721@gmail.com>
2021-05-25 21:20:43 -0400

    Revert "Revert "Revert "Created Insult Command with gTTs"""
    This reverts commit 96685adb9d7492288cf05f5d1c9ac03335323402.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from asyncio.windows_events import NULL
 import discord
-from discord import voice_client
 from discord.ext import commands
 import json
 import os
@@ -12,10 +11,10 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
-intents.members = True
 
-bot = commands.Bot(command_prefix="$", description="A bot", intents=intents)
+
+
+bot = commands.Bot(command_prefix="$", description="A bot")
 
 
 channel_dict = {
@@ -40,90 +39,58 @@ async def ping(ctx, arg):
 @bot.command()
 async def summon(ctx, *args):
     if (args != ()):
-        await joinChat(ctx, args[0])
-    else:
-        await joinChat(ctx, ctx.author.voice.channel.name)
+        try:
+            if (ctx.voice_client is None):
+                channel = bot.get_channel(channel_dict[args[0]])
+                await channel.connect()
+                
+            
+                guild = ctx.guild
+                voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+                tts = gTTS('Walter is fucking retarded')
+                tts.save('BigGay.mp3')
 
+                if not voice_client.is_playing():
+                    voice_client.play(discord.FFmpegPCMAudio('BigGay.mp3'), after=lambda e: print(f"Finished playing: {e}"))
+                
+            else:
+                await ctx.voice_client.disconnect()
+                channel = bot.get_channel(channel_dict[args[0]])
+                await channel.connect()
+
+        except:
+            await ctx.send("Channel does not exist or I'm banned " + find_emote(':sadge:'))
+    else:
+        try:
+            channel = ctx.author.voice.channel
+            await channel.connect()
+        except:
+            await ctx.send("I'm banned" + find_emote(':sadge:'))
 
 @bot.command()
 async def banish(ctx):
-    await leaveChat(ctx)
-
-
-@bot.command()
-async def insult(ctx, *args):     
-    if (args == ()):
-        #author = str(ctx.author)
-        await ctx.send("Your an idiot " + ctx.message.author.mention)
-    else:
-        target = args[0]
-        members = await getMembers(ctx)
-        voiceMembers = getVoiceChatMembers(channel_dict['general'])
-        
-        if (target in voiceMembers):
-            await joinChat(ctx, 'general')
-
-            if ((target in members) and (len(args) != 2)):
-                speak(ctx.guild, (target + ' is Big Gay'))
-            else:
-                speak(ctx.guild, args[1])
-            
-            voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-            
-            while voice_client.is_playing():
-                pass
-            
-            await leaveChat(ctx)
-        else:
-            await ctx.send("Member not in chat " + findEmote(':PepeHands:'))
-
-
-async def getMembers(ctx):
-    memberList = []
-
-    async for member in ctx.guild.fetch_members(limit=None):
-        memberList.append(member.name)
-    
-    return memberList
-
-async def joinChat(ctx, channel):
-    try:
-        if (channel in channel_dict) and (len(bot.voice_clients) == 0):
-            vc = bot.get_channel(channel_dict[channel])
-            await vc.connect()
-        elif((channel in channel_dict) and (len(bot.voice_clients) > 0) and (bot.voice_clients[0].channel.id != channel_dict[channel])):
-            await ctx.voice_client.disconnect()
-            vc = bot.get_channel(channel_dict[channel])
-            await vc.connect()
-        else:
-            await ctx.send("Channel does not exist or I'm already in channel moron" + findEmote(':ree:'))
-    except:
-        await ctx.send("I'm banned " + findEmote(':sadge:'))
-
-async def leaveChat(ctx):
     await ctx.voice_client.disconnect()
 
-
-def getVoiceChatMembers(channel):
-    activeMembers = bot.get_channel(channel)
-    vcm = activeMembers.members
-    members = [] 
-
-    for member in vcm:
-        members.append(member.name)
-
-    return members
-
-def findEmote(name):
+def find_emote(name):
     for emote in emotes:
         if (str(emote).find(name) != -1):
             return str(emote)
 
-def speak(guild, msg):
+##TODO: Must complete function so it can accept user input fore the insult.
+# @bot.command()
+# async def insult(ctx, *args):     
+#     if (args == ()):
+#         #author = str(ctx.author)
+#         await ctx.send("Your an idiot " + ctx.message.author.mention)
+#     else:
+        
+        
+
+def speak(guild):
     voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
-    tts = gTTS(msg)
-    tts.save('text.mp3')
+    tts = gTTS('Walter is fucking retarded')
+    tts.save('BigGay.mp3')
     if not voice_client.is_playing():
-        voice_client.play(discord.FFmpegPCMAudio('text.mp3'), after=lambda e: print(f"Finished playing: {e}"))
+        voice_client.play(discord.FFmpegPCMAudio('BigGay.mp3'), after=lambda e: print(f"Finished playing: {e}"))
 
 bot.run(TOKEN)
